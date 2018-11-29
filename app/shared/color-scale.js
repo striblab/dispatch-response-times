4fadd1a01ca7aab7666e43e57d7c8ef60ba74f02
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-11-29 12:36:30 -0600

    Legend/.

--- app/shared/color-scale.js
+++ app/shared/color-scale.js
@@ -3,9 +3,13 @@
  */
 
 /* global d3 */
+// We use CDN for browser, but want to share this with Node
+if (global && !global.d3 && typeof require !== 'undefined') {
+  global.d3 = require('d3');
+}
 
 // Steps
-let colorSteps = [300, 360, 420, 480];
+let colorSteps = [300, 360, 420, 480, 540];
 
 // Scale
 let colorScale = d3
@@ -14,4 +18,4 @@ let colorScale = d3
   .range(d3.schemeYlGnBu[colorSteps.length + 1]);
 
 // Export
-export { colorSteps, colorScale };
+module.exports = { colorSteps, colorScale };
