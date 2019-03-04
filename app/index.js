d3cf691334f50bb812506094ac9226d9d74138fa
Alan Palazzolo <alan.palazzolo@startribune.com>
2019-03-04 16:52:10 -0600

    Moving map into component.

--- app/index.js
+++ app/index.js
@@ -2,29 +2,17 @@
  * Main JS file for project.
  */
 
-/* global mapboxgl, MapboxGeocoder, $ */
+/* global mapboxgl */
 
 // Dependencies
 import utils from './shared/utils.js';
-import mapConfig from './shared/map-config.js';
-// import Popover from './shared/popover.js';
-// import responseTimeLayer from './shared/response-time-layer.js';
-// import poiLayer from './shared/poi-layer.js';
-
-import drawMap from './shared/draw-map.js';
 
 // Mark page with note about development or staging
 utils.environmentNoting();
 
 // Mapbox access token
-mapboxgl.accessToken = mapConfig.accessToken;
-
-// Some points and area
-const twinCitiesBounds = [
-  [-93.3487129795, 44.8787481227],
-  [-92.9895973789, 45.0618881395]
-];
-const twinCitiesCenter = [-93.191872, 44.960911];
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
 
 // Reload components
 import Content from '../templates/_index-content.svelte.html';
@@ -35,20 +23,3 @@ const app = new Content({
   hydrate: true,
   data: {}
 });
-
-// Draw police map
-drawMap({
-  element: 'police-explorable-map',
-  responseData: 'police',
-  twinCitiesBounds,
-  twinCitiesCenter,
-  addGeocoder: true
-});
-
-// Draw fire map
-// drawMap({
-//   element: 'fire-explorable-map',
-//   responseData: 'fire',
-//   twinCitiesBounds,
-//   twinCitiesCenter
-// });
