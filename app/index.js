cda76f457c5d28efa6efbd170e2fc437a2e1140e
Alan Palazzolo <alan.palazzolo@startribune.com>
2019-03-06 17:09:13 -0600

    Fixing share and geocoder.

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 
 // Dependencies
 import utils from './shared/utils.js';
+import Content from '../templates/_index-content.svelte.html';
 
 // Mark page with note about development or staging
 utils.environmentNoting();
@@ -14,12 +15,24 @@ utils.environmentNoting();
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
 
-// Reload components
-import Content from '../templates/_index-content.svelte.html';
+// Share hack
+let shareEls = [];
+let shareEl = document.querySelector('.share-placeholder');
+while (shareEl.firstChild) {
+  shareEls.push(shareEl.firstChild);
+  shareEl.removeChild(shareEl.firstChild);
+}
+const attachShare = () => {
+  shareEls.forEach(el => {
+    document.querySelector('.share-placeholder').appendChild(el);
+  });
+};
 
 // Main component
-const app = new Content({
+window.__interactiveApp = new Content({
   target: document.querySelector('.article-lcd-body-content'),
   hydrate: true,
-  data: {}
+  data: {
+    attachShare
+  }
 });
